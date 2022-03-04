c188fe1468b1c8c33956211afddf43ea914fc027
Akshay KM <akshay194@gmail.com>
2022-03-04 11:33:40 +0530

    expenses added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItems from "./components/ExpenseItems";
+import Expenses from "./components/Expenses";
 
 
 function App() {
@@ -28,11 +28,7 @@ function App() {
   ];
   return (
     <div className=" bg-orange-600 h-auto p-8">
-      <ExpenseItems titel={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItems>
-      <ExpenseItems titel={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItems>
-      <ExpenseItems titel={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItems>
-      <ExpenseItems titel={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}></ExpenseItems>
-      <ExpenseItems titel={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}></ExpenseItems>
+      <Expenses items={expenses}/>
     </div>
   );
 }
